bc76d9a7f82e08622a59d7aec870b3d07be97438
Saleeshadenuwansl <nonesaliya@gmail.com>
2024-09-28 16:36:12 +0530

    Update config.js

--- config.js
+++ config.js
@@ -6,4 +6,6 @@ function convertToBool(text, fault = 'true') {
 }
 module.exports = {
 SESSION_ID: process.env.SESSION_ID,
+ALIVE_IMG: process.env.ALIVE_IMG || "https://i.ibb.co/2Z7LdLD/20240928-155916.jpg"
+ALIVE_MSG: process.env.ALIVE_MSG || "Hello 👋 I am SL FOX - MD, I am alive now✅ "
 };
